adec8ec992d03de066371a8041c9283275063b4a
Tsuyoshi Okumura <tsuyoshi@freemind.co.jp>
2016-06-01 10:47:44 +0900

    first commit.

---
+++ app/views/entries/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @entry, :id, :title, :body, :tag, :created_at, :updated_at
---
+++ db/migrate/20160601013452_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :entries do |t|
+      t.text :title
+      t.text :body
+      t.text :tag
+
+      t.timestamps
+    end
+  end
+end
